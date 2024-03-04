d4b4dcebff73da90379918fd8aeefdbec278860f
sandeep <you@example.com>
2024-03-04 16:05:22 +0530

    second commot

---
+++ 01_basis/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 12345;
+//accountId = 20; // This will result in "Assignment to constant variable" error
+
+// let accountEmail = "saim251098@gmail.com";
+// var accountPassword = "123456"
+// city = "jaypur"
+
+// accountId = 1  // not allowed
+
+console.log(accountId) 
