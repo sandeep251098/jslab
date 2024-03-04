7dee01dd24fbbf4d3ec6fe5688c254c80b559d75
sandeep kumar <59505502+sandeep251098@users.noreply.github.com>
2024-03-04 13:09:01 +0000

    let-const-var-ex

--- 01_basis/01_variables.js
+++ 01_basis/01_variables.js
@@ -1,10 +1,24 @@
 const accountId = 12345;
-//accountId = 20; // This will result in "Assignment to constant variable" error
 
-// let accountEmail = "saim251098@gmail.com";
-// var accountPassword = "123456"
-// city = "jaypur"
+//accountId = 20; // This will result (error) in "Assignment to constant variable" error
+// accountId = 1  // not allowed  //Assignment to constant variable error
+//console.log(accountId) 
 
-// accountId = 1  // not allowed
+let accountEmail = "saim251098@gmail.com";
+var accountPassword = "123456"
+ accountCity = "jaypur"
+ accountCity ="bangaluru" // result bangaluru reinitialize
 
-console.log(accountId) 
+ accountEmail = "saim251098@gmail";
+ accountPassword = "21212"
+
+
+
+let accountState;// only declare results undefined
+var accountState1; // only declare results undefined
+var accountState2; // only declare results undefined
+/*
+prefer not to use  var
+because of issue is block scope and functional scope
+*/
+console.table([accountEmail,accountPassword,accountCity,accountState,accountState1,accountState2]) 
